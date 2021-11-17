5053357f871fb2fa6cdb7ea3fc0aa83d6be3c499
Danil Klimov <aguhsarvze@protonmail.com>
2021-11-17 14:04:27 +0700

    added debug class

--- Tools/addition_tools.py
+++ Tools/addition_tools.py
@@ -1,18 +1,27 @@
 import os.path
 import yaml.loader
+import pygame
+import soundfile as sf
 
 
 class Based:
-    def __init__(self):
+    def __init__(self, song_path):
         self.deltaA = 0
         self.deltaB = 0
         self._deltaTime = 0
+        self.sampling = 0
+        self.a = pygame.mixer.Sound('audios/{}.wav'.format(song_path))
+        self.data, self.samplerate = sf.read('audios/{}.wav'.format(song_path))
 
     def deltaTime(self, ticks):
         self.deltaA = ticks
         self._deltaTime = (self.deltaA - self.deltaB) / 1000.0
         self.deltaB = self.deltaA
         return self._deltaTime
+
+    def FindSampling(self):
+        self.sampling = round((len(self.data) / self.a.get_length() / 1000), 0)
+        return self.sampling
 
 
 class Parser:
@@ -28,3 +37,40 @@ class Parser:
         if name in self.opened:  # check accordance of dict element with param
             return self.opened[name]  # returned if successful
         return 'nothing found'  # returned if error
+
+
+class DEBUG:
+    def __init__(self, song, font, screen):
+        self.fps = 0
+        self.seconds = 0
+        self.fps_text = ""
+        self.sampling_text = ""
+        self.timer = ""
+        self.time_text = ""
+        self.font = font
+        self.screen = screen
+        self.all_timer_secs = int(round(song.get_length(), 0))
+        self.all_timer = str(self.all_timer_secs // 60) + " : " + str(self.all_timer_secs - (60 * (self.all_timer_secs // 60)))
+
+    def showFPS(self, clock):
+        self.fps = "FPS:   " + str(int(clock.get_fps()))
+        self.fps_text = self.font.render(self.fps, 1, pygame.Color("white"))
+        self.screen.blit(self.fps_text, (10, 10))
+
+    def showSampling(self, sampling):
+        self.sampling_text = self.font.render(("sampling is:   " + str(int(sampling * 1000))), 1, pygame.Color('white'))
+        self.screen.blit(self.sampling_text, (10, 40))
+
+    def showTimer(self, mus_pos):
+        self.seconds = int(round((mus_pos / 1000), 0))
+        self.timer = str(self.seconds // 60) + " : " + str(self.seconds - (60 * (self.seconds // 60)))
+        self.time_text = self.font.render(("time:   " + self.timer + "   of   " + self.all_timer), 1, pygame.Color('white'))
+        self.screen.blit(self.time_text, (10, 70))
+
+    def showWave(self, amplitude):
+        self.wave_text = self.font.render(("wave amplitude:   " + str(round(amplitude, 2))), 1, pygame.Color('white'))
+        self.screen.blit(self.wave_text, (10, 100))
+
+    def showFrame(self, audiotime, data):
+        self.longest_text = self.font.render(("frames:   " + str(audiotime) + " of " + str(len(data))), 1, pygame.Color('white'))
+        self.screen.blit(self.longest_text, (10, 130))
--- main.py
+++ main.py
@@ -7,10 +7,8 @@ import Tools.addition_tools as adt
 pygame.init()
 
 
-
 class Star:
     def __init__(self, app):
-        self.screen = app.screen
         self.pos3d = self.get_pos3d()
         self.vel = random.uniform(0.1, 1.0)
         self.color = random.choice(settings['COLORS'])
@@ -40,13 +38,12 @@ class Star:
         self.screen_pos += mouse_pos
 
     def draw(self):
-        pygame.draw.rect(self.screen, self.color, (*self.screen_pos, self.size, self.size))
+        pygame.draw.rect(screen, self.color, (*self.screen_pos, self.size, self.size))
 
 
 class Starfield:
     def __init__(self, app):
         self.stars = [Star(app) for i in range(settings['NUMBER OF STARS'])]
-
     def run(self):
         [star.update() for star in self.stars]
         self.stars.sort(key=lambda star: star.pos3d.z, reverse=True)
@@ -59,36 +56,18 @@ class App:
         pygame.mixer.music.load('audios/{}.wav'.format(settings['SONG NAME']))
         self.a = pygame.mixer.Sound('audios/{}.wav'.format(settings['SONG NAME']))
         self.data, self.samplerate = sf.read('audios/{}.wav'.format(settings['SONG NAME']))
-        self.screen = pygame.display.set_mode(RES)
         self.alpha_surface = pygame.Surface(RES)
         self.alpha_surface.set_alpha(settings['TRAIL ALPHA'])
         self.clock = pygame.time.Clock()
-        self.font = pygame.font.SysFont("Arial", 18)
         self.starfield = Starfield(self)
         self.fps = 0
         self.fps_text = ""
         self.wave_text = ""
         self.audiotime = 0
         self.amplitude = 0
-        self.all_timer_secs = int(round(self.a.get_length(), 0))
-        self.all_timer = str(self.all_timer_secs // 60) + " : " + str(self.all_timer_secs - (60 * (self.all_timer_secs // 60)))
         self.sampling = round((len(self.data) / self.a.get_length() / 1000), 0)
-        self.sampling_text = self.font.render(("sampling is:   " + str(int(self.sampling * 1000))), 1, pygame.Color('white'))
         pygame.mixer.music.play(-1)
 
-        print(self.data)
-
-        print("length is: " + str(self.a.get_length()))
-
-    def DEBUG(self):
-        self.fps = "FPS:   " + str(int(self.clock.get_fps()))
-        self.fps_text = self.font.render(self.fps, 1, pygame.Color("white"))
-        self.wave_text = self.font.render(("wave amplitude:   " + str(self.amplitude)), 1, pygame.Color('white'))
-        self.longest_text = self.font.render(("frames:   " + str(self.audiotime) + " of " + str(len(self.data))), 1, pygame.Color('white'))
-        self.seconds = int(round((pygame.mixer.music.get_pos() / 1000), 0))
-        self.timer = str(self.seconds // 60) + " : " + str(self.seconds - (60 * (self.seconds // 60)))
-        self.time_text = self.font.render(("time:   " + self.timer + "   of   " + self.all_timer), 1, pygame.Color('white'))
-        return self.fps_text, self.wave_text, self.longest_text, self.time_text
 
     def run(self):
         while True:
@@ -96,20 +75,21 @@ class App:
             self.amplitude = abs(round((self.data[self.audiotime, 0] * settings['AMPLITUDE COEFFICIENT']), 9))
             print(str(self.audiotime) + " : " + str(self.amplitude))
             # self.screen.fill('black')
-            self.screen.blit(self.alpha_surface, (0, 0))
+            screen.blit(self.alpha_surface, (0, 0))
 
             if settings['IS DEBUG MODE']:
-                self.screen.blit(self.DEBUG()[0], (10, 10))
-                self.screen.blit(self.sampling_text, (10, 40))
-                self.screen.blit(self.DEBUG()[1], (10, 70))
-                self.screen.blit(self.DEBUG()[2], (10, 100))
-                self.screen.blit(self.DEBUG()[3], (10, 130))
+                debug.showFPS(self.clock)
+                debug.showSampling(self.sampling)
+                debug.showTimer(pygame.mixer.music.get_pos())
+                debug.showFrame(self.audiotime, self.data)
+
 
             self.starfield.run()
 
             pygame.display.flip()
             for i in pygame.event.get():
                 if i.type == pygame.QUIT:
+                    print(self.starfield.stars)
                     exit()
 
             self.clock.tick(settings['FRAMERATE'])
@@ -119,10 +99,15 @@ if __name__ == "__main__":
 
     parse_settings = adt.Parser('settings.yaml')
     settings = parse_settings.opened
+
+    RES = settings['WIDTH'], settings['HEIGHT']
+    global_font = pygame.font.SysFont("Arial", 18)
+    screen = pygame.display.set_mode(RES)
+
     based = adt.Based()
+    if settings['IS DEBUG MODE']: debug = adt.DEBUG(pygame.mixer.Sound('audios/{}.wav'.format(settings['SONG NAME'])), global_font, screen)
 
     vec2, vec3 = pygame.math.Vector2, pygame.math.Vector3
-    RES = settings['WIDTH'], settings['HEIGHT']
     CENTER = vec2(RES[0] // 2, RES[1] // 2)
 
     app = App()
